c4c46206deb60cccfdf7888f0171cffa217c731d
AshDhe <157318874+AshDhe@users.noreply.github.com>
2026-07-20 09:44:36 +0200

    Add files via upload

--- ESPACE-MEMBRE/box-menu-burger-membre.js
+++ ESPACE-MEMBRE/box-menu-burger-membre.js
@@ -1002,7 +1002,7 @@
 
     const liens = [
       {
-        label: "La Clé du Parc",
+        label: "Accueil La Clé du Parc",
         espace: "membre",
         href: "/ESPACE-MEMBRE/accueil-membre.html"
       }
